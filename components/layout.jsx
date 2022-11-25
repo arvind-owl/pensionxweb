d6fb3c0b99646253572ba87ee1c1f773b744098f
Ravinder Kumar <ravinder@euclidesolutions.co.in>
2022-11-25 19:04:18 +0530

    Update CHanges

--- components/layout.jsx
+++ components/layout.jsx
@@ -19,8 +19,143 @@ export default function Layout({ children, footerMenu, headerMenu }) {
 		<>
 		<div className="min-h-screen max-h-screen min-w-screen max-w-screen flex flex-col">
 			{/* <Loader /> */}
-			<HeaderTop />
-			<Header headerMenu={headerMenuItems} />
+			{/* <HeaderTop />
+			<Header headerMenu={headerMenuItems} /> */}
+			 <div className="header-top-bar text-white d-none d-sm-block">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 text-end text-sm-center">
+            <ul className="top-left-content">
+              <li>
+                <i className="fa fa-calendar"></i>
+                <b>Pension Payment Calendar</b>
+                <span className="check-mailed"> - Check Mailed <b>JULY 29</b>
+                </span>
+              </li>
+              <li className="direct-deposit"> Direct Deposit <b>JULY 31</b>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+    <header id="header">
+      <div className="container display-none-t">
+        <div className="row">
+          <div className="header_set">
+            <div className="col-md-12 text-right">
+              <div className="get-tuch text-left">
+                <div className="bs-searchbox">
+                  <input type="text" className="form-control" placeholder="Search" />
+                </div>
+              </div>
+              <div className="get-tuch text-left">
+                <button type="submit" className="btn_fill">Member Login/Registration</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav className="navbar navbar-default navbar-fixed no-background navbar-sticky dark bootsnav">
+        <div className="container">
+        
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+              <i className="fa fa-bars"></i>
+            </button>
+            <a className="navbar-brand" href="index.html">
+              <img src="/img/logo.png" className="img-fluid desktop-only" alt="logo" />
+              <img src="/img/mobile-logo.png" className="img-fluid mobile-only" alt="logo" />
+            </a>
+          </div>
+        
+          <div className="collapse navbar-collapse nav_bor_bot" id="navbar-menu">
+            <ul className="nav navbar-nav navbar-right nav_3" data-in="fadeInDown" data-out="fadeOutUp">
+              <li className="mobile-only">
+                <div className="get-tuch text-left">
+                  <div className="bs-searchbox">
+                    <input type="text" className="form-control" placeholder="Search" />
+                  </div>
+                </div>
+              </li>
+              <li className="mobile-only">
+                <div className="get-tuch text-left">
+                  <button type="submit" className="btn_Translate">실A Translate</button>
+                </div>
+              </li>
+              <li className="dropdown mobile-only">
+                <a href="board.html" className="dropdown-toggle" data-toggle="dropdown">Board of retirements</a>
+                <ul className="dropdown-menu animated">
+                  <li>
+                    <a href="#">Board of retirements - 1</a>
+                  </li>
+                  <li>
+                    <a href="#">Board of retirements - 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Board of retirements - 3</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a className="list-items" href="board-information.html">Board of retirements</a>
+              </li>
+              <li>
+                <a className="list-items" href="investments.html">Investments</a>
+              </li>
+              <li>
+                <a className="list-items" href="finance.html">Finance</a>
+              </li>
+              <li>
+                <a className="list-items" href="news.html">News & Media</a>
+              </li>
+              <li>
+                <a className="list-items" href="about.html">About</a>
+              </li>
+              <li>
+                <a className="list-items" href="contact.html">Contact</a>
+              </li>
+              <li className="dropdown desktop-only">
+                <a className="list-items dropdown-toggle" href="#"  data-toggle="dropdown"> 실A Translate </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a href="#">English</a>
+                  </li>
+                  <li>
+                    <a href="#">Spanish</a>
+                  </li>
+                  <li>
+                    <a href="#">French</a>
+                  </li>
+                  <li>
+                    <a href="#">Lorem Ipsum</a>
+                  </li>
+                  <li>
+                    <a href="#">Lorem Ipsum</a>
+                  </li>
+                  <li>
+                    <a href="#">Lorem Ipsum</a>
+                  </li>
+                  <li>
+                    <a href="#">Lorem Ipsum</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <section className="registration-section mobile-only">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <button type="submit" className="btn_fill">Member Login/Registration</button>
+          </div>
+        </div>
+      </div>
+    </section>
 			<div className="hero_slider padding-bottom-top-120">
 				<div className="container">
 					<div className="row">
