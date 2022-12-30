8c91803db6f791d5107cd1e9cfd94f75937a0f23
Ravinder Kumar <ravinder@euclidesolutions.co.in>
2022-12-30 16:15:41 +0530

    Update

--- pages/calendar/customEvent.jsx
+++ pages/calendar/customEvent.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Link from 'next/link';
-export default function CustomEvent (props) {
+ const CustomEvent = (props) => {
     return (
         <div>
             <Link href={props && props.linkhref} ><a>{props.title}</a></Link>
         </div>
     );
 }
+export default CustomEvent;
